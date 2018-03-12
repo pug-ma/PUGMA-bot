3eb4e3019ee7a5d48761ff4608423326acc82ec0
Ronald-TR <ronald-farias@outlook.com>
2018-03-12 11:54:10 -0300

    removendo 'with as' desnecessario em PugBot

--- app/core/pugbot.py
+++ app/core/pugbot.py
@@ -8,18 +8,18 @@ PARAGRAPH = lambda k, v: BOLD(k) + ': ' + v
 class PugBot():
     """
     PugBot() irá recolher da .env a URL para um arquivo JSON
-    contendo um array com objetos referenciados aos eventos
-    o metodo lastEvent retorna um dict com dois elementos
+    contendo um array com objetos referenciados aos eventos,
+    o metodo lastEvent retorna um dict com dois elementos:
 
     text: uma saida valida em HTML das infos
     photo: URL do banner utilizado para o meetup 
     """
     def __init__(self):
-        with os.getenv('RAW_GIT_DOC_URL') as url:
-            if not url:
-                raise Exception('Unable to load the events data, empty source URL')
-            self.URL = url
-            self.DOC_INFO = json.loads(requests.get(self.URL).content) 
+        url = os.getenv('RAW_GIT_DOC_URL')
+        if not url:
+            raise Exception('Unable to load the events data, empty source URL')
+        self.URL = url
+        self.DOC_INFO = json.loads(requests.get(self.URL).content) 
     
     def _get_last_event(self): 
         return self.DOC_INFO[-1]
