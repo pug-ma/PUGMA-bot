1931ccde7e617fbb2d65c5aea0cf2351cf6d0fc9
Luiz <luizrodrigo46@hotmail.com>
2018-05-29 18:13:05 -0300

    logging settings added

--- app/settings.py
+++ app/settings.py
@@ -1,6 +1,19 @@
 from decouple import config
+import logging
 
-
+# Bot keys settings
 API_KEY = config('TOKEN')
 APP_NAME = config('APP_NAME')
 PORT = config('PORT', default='8443', cast=int)
+
+
+# Logging settings
+DEBUG = config('DEBUG', default=False, cast=bool)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG if DEBUG else logging.NOTSET)
