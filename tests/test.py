c042b984daebd5901a7dd7b01fd0796c2f49eabc
Lucas Souto <lucasinfologos@gmail.com>
2018-08-25 04:45:55 -0300

    Add tests API Github

--- tests/test.py
+++
@@ -1,8 +0,0 @@
-from unittest import TestCase, main
-
-class TestBot(TestCase):
-    def test_basic(self):
-        self.assertEquals(1, 1)
-
-if __name__ == '__main__':
-    main()
---
+++ tests/tests_github.py
@@ -0,0 +1,20 @@
+import requests
+import json
+from app.api_github import Github 
+
+
+class TestGithub:
+    user = 'pug-ma'
+    repository = 'meetups'
+    base_url = 'https://api.github.com/repos'
+        
+    def test_repository(self):
+        response = requests.get(f'{self.base_url}/{self.user}/{self.repository}')
+        
+        assert 200 == response.status_code
+    
+    def test_encontros_download_url(self):
+        download_url = 'https://raw.githubusercontent.com/pug-ma/meetups/master/encontros/PUG-MA%20%2302.jpg'
+        api = Github()
+
+        assert download_url == api.photo_encontro('2')
