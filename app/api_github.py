9b01df7e1dbf2ea24fa64990d563e94e96303ebf
Lucas Souto <lucasinfologos@gmail.com>
2018-08-25 04:47:04 -0300

    Add module api_github.py and method photo_encontro

---
+++ app/api_github.py
@@ -0,0 +1,22 @@
+import requests
+import json
+
+
+class Github:
+    def __init__(self):
+        self.user = 'pug-ma'
+        self.repository = 'meetups'
+        self.base_url = 'https://api.github.com/repos'
+
+    def _name_encontro(self, index):
+        if len(index) == 1:
+            index = '0' + index
+
+        return requests.utils.quote(f'PUG-MA #{index}.jpg')
+
+    def photo_encontro(self, index):
+        url = f'{self.base_url}/{self.user}/{self.repository}/contents/encontros/{self._name_encontro(index)}'
+
+        response = requests.get(url)
+        content = json.loads(response.content)
+        return content.get('download_url')
